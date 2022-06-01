c4bf4da3bbb42a1f47611541a1a944b4e9ef2750
Diogenes Rychlewski <dirychlewski@gmail.com>
2022-06-01 17:46:42 -0300

    Componentes e Paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/Subscribe Button';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>Início | IgNews</title>
+      <title>Home | IgNews</title>
     </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the<span> world.</span></h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.98 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
